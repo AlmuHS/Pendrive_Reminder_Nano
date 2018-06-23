32ba9c3972023780f84ae5b4b9de5b708f311617
AlmuHS <liberamenso10000@gmail.com>
2018-06-23 20:49:48 +0200

    added xdg autostart launcher for dbus client

--- installer.sh
+++ installer.sh
@@ -5,30 +5,32 @@ if [[ $EUID -ne 0 ]]; then
    echo "This script must be run as root" 
    exit 1
 else
-	INSTALL_DIR="/usr/bin/pendrive-reminder"
-
-	#Copy auxiliar scripts
-	mkdir $INSTALL_DIR 2>/dev/null
-	cp -r aux_scripts/ $INSTALL_DIR
-	chmod +x $INSTALL_DIR/aux_scripts/*
-
-	#Copy env variables into a auxiliar file
-	echo "DISPLAY="$DISPLAY"" > $INSTALL_DIR/var
-	echo "LANG="$LANG"" >> $INSTALL_DIR/var
-
-	#Copy locale files
-	cp -r locale/* /usr/share/locale/
-
-	#Copy polkit rules
-
 	#If polkit version is >= 0.106
 	if test $(pkaction --version | cut -d " " -f 3 | cut -d "." -f 2) -ge 106
-	then
+	then	
+
+		INSTALL_DIR="/usr/bin/pendrive-reminder"
+
+		#Copy auxiliar scripts
+		mkdir $INSTALL_DIR 2>/dev/null
+		cp -r aux_scripts/ $INSTALL_DIR
+		chmod +x $INSTALL_DIR/aux_scripts/*
+
+		#Copy locale files
+		cp -r locale/* /usr/share/locale/
+
+		#Copy polkit rules
 		#copy rules file	
 		cp polkit-rules/10-inhibit-shutdown.rules /usr/share/polkit-1/rules.d/
 
 		#copy dbus-client
 		cp -r dbus-client/ $INSTALL_DIR
 		chmod ugo+x $INSTALL_DIR/dbus-client/client.py
+
+		#Copy xdg launcher for dbus client
+		cp xdg-launcher/preminder.desktop /etc/xdg/autostart
+
+	else
+		echo "This polkit version is not supported by Pendrive Reminder Nano. Download Pendrive Reminder from https://github.com/AlmuHS/Pendrive_Reminder"
 	fi
 fi
--- uninstaller.sh
+++ uninstaller.sh
@@ -14,25 +14,10 @@ else
 	#Remove locale files
 	find /usr/share/locale/ -name "preminder*" -delete
 
-	#If polkit version is >= 0.106, remove rules and policy file	
-	if test $(pkaction --version | cut -d " " -f 3 | cut -d "." -f 2) -ge 106
-	then
-		rm /usr/share/polkit-1/rules.d/10-inhibit-shutdown.rules
-		
-		#if there are any dbus client active, kill them
-		if test -f /tmp/pid_dbus
-		then
-			while read pid
-			do
-				kill -9 $pid
-			done < /tmp/pid_dbus
+	#Remove polkit rules and policy file	
+	rm /usr/share/polkit-1/rules.d/10-inhibit-shutdown.rules
 
-			#Remove temporary file
-			rm /tmp/pid_dbus
-		fi
-
-	if test -f /tmp/usbdevinfo
-	then
-		rm /tmp/usbdevinfo
-	fi
+	#Remove xdg autostart entrt
+	rm /etc/xdg/autostart/preminder.desktop
+	
 fi
