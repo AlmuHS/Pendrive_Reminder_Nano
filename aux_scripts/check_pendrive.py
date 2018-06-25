7dcb091958ad07be0ff2fa1b962f57bd3d76d504
AlmuHS <liberamenso10000@gmail.com>
2018-06-26 01:52:07 +0200

    added documentation

--- aux_scripts/check_pendrive.py
+++ aux_scripts/check_pendrive.py
@@ -1,5 +1,18 @@
 #!/usr/bin/python3
 
+#Script linked to 10-inhibit-shutdown polkit rule
+#This script check if there are any usb mass storage device connected in the system
+
+#To get this, the script execute a udisks2 query, using dbus and udisks2-api
+
+#If there are any pendrive connected -> return 1 (error)
+#else -> return 0 (correct)
+
+#This value is returned to polkit using sys.exit()
+
+#Based in https://askubuntu.com/questions/437031/finding-volume-label-of-a-usb-mass-storage-device-using-python
+
+
 import dbus
 import sys
 
@@ -8,8 +21,6 @@ def get_usb():
     bus = dbus.SystemBus()
     ud_manager_obj = bus.get_object('org.freedesktop.UDisks2', '/org/freedesktop/UDisks2')
     om = dbus.Interface(ud_manager_obj, 'org.freedesktop.DBus.ObjectManager')
-
-    counter = 0;
 
     for k,v in om.GetManagedObjects().items():
         drive_info = v.get('org.freedesktop.UDisks2.Drive', {})
